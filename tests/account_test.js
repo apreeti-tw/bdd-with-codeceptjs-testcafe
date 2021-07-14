8225ee7ed4992d5b3a8ce298dad9643f2a5f04d8
apreeti-tw <apreeti@thoughtworks.com>
2021-07-14 13:38:53 +0530

    Use I.say as a description for the following step

--- tests/account_test.js
+++ tests/account_test.js
@@ -6,8 +6,11 @@ Before(({I}) => {
 })
 
 Scenario('User should be able to add new address', ({I, accountPage}) => {
+  I.say('User enters address details')
   accountPage.sendAddressDetails(userdata.user)
   I.waitForText(alertmessages.newAddressSaveSuccessMsg)
+
+  I.say('Verify if user address is displayed as entered')
   accountPage.checkIfAddressDisplayed(userdata.user)
 })
 
